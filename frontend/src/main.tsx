e8cf4e1e146bdd1c41e0e6e85a3a947bed7e693d
JoshYadav <joshyadav14062006@gmail.com>
2026-06-11 23:31:05 +0530

    chore: remove debug lines from production code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-// Production build - force env var reload
 // Unregister any lingering service workers from development
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.getRegistrations().then((registrations) => {
